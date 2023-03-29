eb00f722aa532a2f56d45555a683e4f2083ac88b
Dinh Nguyen <nguyen_dinh282@yahoo.com>
2023-03-29 00:44:32 -0400

    add code to generatePassword function

--- Develop/script.js
+++ Develop/script.js
@@ -8,7 +8,53 @@ function writePassword() {
 
   passwordText.value = password;
 
+
+  
+
+
+function generatePassword() {
+
+  var lowerCaseChars = confirm("Would you like to include lowercase characters in your password?");
+  var upperCaseChars = confirm("would you like to include uppercase characters in your password?");
+  var numbers = confirm("Would you like to include number characters in your password?");
+  var specialChars = confirm("Would you like to include special characters?");
+  var passLength = prompt("How many characters would you want your password to include? *Note: Pick between 8 and 128 characters");
+  
+  
+  while (isNan(passLength) || passLength < 8 || passLength > 128) {
+     passLength = prompt("Input not accepted. Please enter a number between 8 and 128!");
+  } 
+
+  var userSelection = [];
+
+   if (lowerCaseChars) {
+     userSelection.push("abcdefghijklmnopqrstuvwxyz");
+   };
+
+   if (upperCaseChars) {
+     userSelection.push("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+   }
+
+  if (numbers) {
+     userSelection.push("0123456789");
+  }
+
+   // the list of special chars is !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~, but we must use the two extra \ \ to escape the string for the " marks.  
+   if (specialChars) {
+    userSelection.push("!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~")
+   }
+
+   if (userSelection.length === 0) {
+     userSelection.alert("You must choose at least one character type.");
+     return;
+   }
+
+
 }
+
+}
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
